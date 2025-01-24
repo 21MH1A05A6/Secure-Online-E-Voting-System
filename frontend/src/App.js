b4a1ceffd3dea70c4ca21d79b72277273ebb9d22
Sindhujasri K <97579846+21MH1A05A6@users.noreply.github.com>
2025-01-24 12:19:32 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import RegistrationPage from './pages/RegistrationPage';
+import LoginPage from './pages/LoginPage';
+import PersonalDetailsPage from './pages/PersonalDetailsPage';
+import HomePage from './pages/HomePage';
+import VoterIDSearchPage from './pages/VoterIDSearchPage';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/registerpage" element={<RegistrationPage />} />
+        <Route path="/loginpage" element={<LoginPage />} />
+        <Route path="/details" element={<PersonalDetailsPage />} />
+        <Route path="/searchpage" element={<VoterIDSearchPage />} />
+      </Routes>
+   
+    </Router>
   );
 }
 
-export default App;
+export default App;
