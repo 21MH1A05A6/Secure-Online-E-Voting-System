9e4ccff1061f43d0e070e752bd2f093cfef8aefc
Sindhujasri K <97579846+21MH1A05A6@users.noreply.github.com>
2025-02-19 22:09:06 +0530

    Updated User.js

--- Backend/models/User.js
+++ Backend/models/User.js
@@ -1,9 +1,12 @@
 const mongoose = require("mongoose");
-
+const bcrypt = require("bcryptjs");
 const UserSchema = new mongoose.Schema({
   username: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  tempPassword: { type: String, default: null }, 
+  tempPasswordExpiry: { type: Date, default: null },
 });
 
 module.exports = mongoose.model("User", UserSchema);
+
