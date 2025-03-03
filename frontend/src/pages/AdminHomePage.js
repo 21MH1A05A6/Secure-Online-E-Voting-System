fd7ca7eb845e4f2f06aa87f2931fc7d5fe96b3c2
Thandava krishna <96812377+Thandava3114@users.noreply.github.com>
2025-03-03 12:20:42 +0530

    Update AdminHomePage.js

--- frontend/src/pages/AdminHomePage.js
+++ frontend/src/pages/AdminHomePage.js
@@ -5,18 +5,39 @@ import "../assets/css/admin-home.css";
 const AdminHomePage = () => {
   const navigate = useNavigate();
 
+  const LogoutButton = () => {
+    // Clear the localStorage data (token and user)
+    localStorage.removeItem("token");
+    localStorage.removeItem("user");
+
+    // Redirect the user to the login page
+    navigate("/loginpage");
+  };
+
   return (
     <div className="home-container">
       <div className="greeting">Welcome, Admin!</div>
       <div className="button-container">
-        <button className="home-button" onClick={() => navigate("/create-election")}>
+        <button
+          className="home-button"
+          onClick={() => navigate("/create-election")}
+        >
           Create Election
         </button>
-        <button className="home-button" onClick={() => navigate("/view-voters")}>
+        <button
+          className="home-button"
+          onClick={() => navigate("/view-voters")}
+        >
           View Voters
         </button>
-        <button className="home-button" onClick={() => navigate("/declare-results")}>
+        <button
+          className="home-button"
+          onClick={() => navigate("/declare-results")}
+        >
           Declare Results
+        </button>
+        <button className="home-button" onClick={LogoutButton}>
+          Logout
         </button>
       </div>
     </div>
