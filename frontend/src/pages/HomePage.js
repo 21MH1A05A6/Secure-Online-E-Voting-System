0750f03c79339a40d2c21d1667503fb0b10a3836
Thandava krishna <96812377+Thandava3114@users.noreply.github.com>
2025-03-03 12:21:58 +0530

    Update HomePage.js

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,23 +1,23 @@
-import React from 'react';
-import Navbar from '../components/Navbar'; // Import the Navbar component
-import Footer from '../components/Footer'; // Import the Footer component
-
-import ImageCarousel from '../components/ImageCarousel';
+import React from "react";
+import Navbar from "../components/Navbar"; // Import the Navbar component
+import backgroundImage from "../assets/images/home-g.webp";
 
 export default function HomePage() {
- 
   return (
-      <div >
-          <div><Navbar /></div>
-          <div><ImageCarousel/></div>
-          
-             
-              
-
-          
-          <div><Footer /></div>
-          {/* <div>Body</div> */}
-      </div>
-
-  )
+    <div
+      style={{
+        background: `linear-gradient(to right, rgba(0, 0, 20, 0.11), rgba(0, 0, 10, 0.26)), url(${backgroundImage})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        width: "100vw",
+        height: "100vh",
+        display: "flex",
+        flexDirection: "column" /* Stack elements vertically */,
+      }}
+    >
+      {/* Navbar */}
+      <Navbar />
+    </div>
+  );
 }
