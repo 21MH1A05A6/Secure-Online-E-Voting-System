bfa55b29937af63c410a0604fe99caf4e1ae04d3
SUTRAYETH HARI YUKTHA NANDA <97426182+21MH1A05C9@users.noreply.github.com>
2025-04-14 10:33:12 +0530

    Update electionRoutes.js

--- Backend/routes/electionRoutes.js
+++ Backend/routes/electionRoutes.js
@@ -1,16 +1,20 @@
 // routes/electionRoutes.js
-const express = require('express');
-const multer = require('multer');
-const path = require('path');
-const { createElection, voteForCandidate } = require('../controllers/electionController');
+const express = require("express");
+const multer = require("multer");
+const path = require("path");
+const {
+  createElection,
+  voteForCandidate,
+  declareResults,
+} = require("../controllers/electionController");
 
 // Storage configuration for multer (save files to 'uploads' directory)
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, 'uploads/');  // Directory where files will be stored
+    cb(null, "uploads/"); // Directory where files will be stored
   },
   filename: (req, file, cb) => {
-    cb(null, Date.now() + path.extname(file.originalname));  // Unique filename using timestamp
+    cb(null, Date.now() + path.extname(file.originalname)); // Unique filename using timestamp
   },
 });
 
@@ -18,6 +22,7 @@ const upload = multer({ storage: storage }).any();
 const router = express.Router();
 
 // POST request for creating election with file uploads
-router.post('/', upload, createElection);
-router.post('/vote',voteForCandidate);
+router.post("/", upload, createElection);
+router.post("/vote", voteForCandidate);
+router.post("/declare-results", declareResults);
 module.exports = router;
