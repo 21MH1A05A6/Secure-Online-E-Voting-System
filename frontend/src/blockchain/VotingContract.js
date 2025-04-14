2fd81c0cdfec428df77c9e848fda926a3c8f44e1
SUTRAYETH HARI YUKTHA NANDA <97426182+21MH1A05C9@users.noreply.github.com>
2025-04-14 10:48:42 +0530

    Update VotingContract.js

--- frontend/src/blockchain/VotingContract.js
+++ frontend/src/blockchain/VotingContract.js
@@ -1,12 +1,13 @@
 import web3 from "./web3";
 import Voting from "./Voting.json";
 
-
-const contractAddress ="0x02651CDDC343ee45556903c69eDCa9faCb226558"; 
+const contractAddress = "0x1d4Bf21841B6B78C99060872A62E5C5605Eb478d";
 
 // Ensure web3 is initialized before using the contract
 if (!web3) {
-  console.error("Web3 is not initialized. Make sure MetaMask is installed and connected.");
+  console.error(
+    "Web3 is not initialized. Make sure MetaMask is installed and connected."
+  );
 }
 
 const VotingContract = new web3.eth.Contract(Voting.abi, contractAddress);
